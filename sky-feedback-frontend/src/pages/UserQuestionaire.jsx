0550c8c3f9705ef84a5651430e8d789a69f79b73
Panya <panya.rattanarom@sky.uk>
2025-10-09 17:07:50 +0100

    MODIFY: add linking backend

--- sky-feedback-frontend/src/pages/UserQuestionaire.jsx
+++ sky-feedback-frontend/src/pages/UserQuestionaire.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { useParams } from "react-router";
-
+import axios from "axios";
 const UserQuestionaire = () => {
   const [answers, setAnswers] = useState({});
   const { id } = useParams();
+  const [questions, setQuestions] = useState();
   const dummyData = {
     title: "Bamjam",
     questions: [
@@ -14,8 +15,19 @@ const UserQuestionaire = () => {
   };
 
   useEffect(() => {
-    console.log(answers);
+    axios
+      .get(`http://localhost:5000/api/questionnaire/${id}`)
+      .then((response) => {
+        setQuestions(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [answers]);
+
+  useEffect(() => {
+    console.log(questions);
+  }, [questions]);
 
   const handleChange = (id, value) => {
     setAnswers((prev) => ({
@@ -46,23 +58,25 @@ const UserQuestionaire = () => {
   return (
     <div className="flex w-full h-full flex-col bg-white text-black justify-center items-center md:justify-start md:items-start">
       <form className="w-full h-full flex flex-col gap-10 lg:w-1/2 p-5">
-        <p className="text-4xl font-light"> {dummyData.title} </p>
+        <p className="text-4xl font-light">
+          {" "}
+          {questions?.questionnaire[0].title}{" "}
+        </p>
         <div className="flex flex-col gap-5 w-full md:w-1/2">
           <TextInput label="Username" name="username" />
           <TextInput label="Name" placeholder="e.g. bryan" name="name" />
         </div>
-        {dummyData.questions.map((item, index) => {
+        {questions?.questions.map((item, index) => {
           return (
-            <>
+            <Fragment key={index}>
               <p className="font-bold text-gray-500 text-2xl">{item.title}</p>
               <textarea
-                key={index}
                 type="text"
                 name="answer"
                 className="h-40 md:h-20 lg:h-60 border border-gray-300 rounded-sm p-5 "
                 onChange={(e) => handleChange(index, e.target.value)}
               />
-            </>
+            </Fragment>
           );
         })}
       </form>
